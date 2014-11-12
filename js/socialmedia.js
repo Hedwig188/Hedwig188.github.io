ca76b5fdbbbb21e86c9144e65464ec7872e85b44
yawei <yaweihua@usc.edu>
2014-11-12 01:34:31 -0800

    fix pinterest widget conflict with query tab bugs

--- js/socialmedia.js
+++ js/socialmedia.js
@@ -1,43 +1,92 @@
-var flag = 1;
-var pinterest_link;
-var pinterest_type;
-var pinterest_tag;
- $(document).ready(function() {
- 	// if(flag == 2){
- 	// 	$('#recent_content').find('a').attr('href','http://www.pinterest.com/racemill/'+tag);
-		// $('#recent_content').find('a').attr('data-pin-do','embedBoard');
-		// flag = 1;
- 	// }
- 	// alert(flag);
- 	if(flag == 2){
- 		$('#recent_content').find('div').attr('display','none');
- 		flag = 1;
- 	}
- });
+
+var media_id;
+var user_id;
+var media_href = 'http://instagram.com/p/u_kRRNNUwT/';
+var title;
+var array = [];
+GetImageDetails();
+$(document).ready(function(){
+    setTimeout(function(){
+        $('#strength_content').css({"display":"none","visibility":"visible"});
+    },2000);
+});
+
+$('#instafeed').click(function(event){
+            
+    media_href = event.target.id;
+
+    GetImageDetails();
+   
+});
+$('#pinterest_a').click(function(){
+    $('#strength_content').show();
+});
+
+$('#instagram_a').click(function(){
+    // alert("n");
+    $('#strength_content').hide();
+});
 
  var userFeed = new Instafeed({
         get: 'user',
         userId: 274348410,
         accessToken: '274348410.467ede5.c610e8b06c5645c29b92084060074a95',
         useHttp: true,
-        template: '<a href="{{link}}embed" target="ins_content"><img src="{{image}}"/></a>',
-        success:function({{model.data}}){
-
-        }
-        
+        template: '<a href="{{link}}embed" target="ins_content" class="instafeed_a"><img src="{{image}}" id="{{link}}"/></a>',
         
     });
-    userFeed.run();
+    userFeed.run();  
+  
+//Get user details  
+        function GetUserDetails() {  
+            $.ajax({  
+                type: "GET",  
+                async: true,  
+                contentType: "application/json; charset=utf-8", 
+                url :'https://api.instagram.com/v1/media/'+array[1]+'/comments?access_token=274348410.467ede5.c610e8b06c5645c29b92084060074a95',
+                // url: 'https://api.instagram.com/v1/users/self/feed?access_token=274348410.467ede5.c610e8b06c5645c29b92084060074a95', 
+                // url:'https://api.instagram.com/v1/users/'+274348410+'/?access_token=274348410.467ede5.c610e8b06c5645c29b92084060074a95', 
+                dataType: "jsonp",  
+                cache: false, 
+                success: function (data) { 
+                    $('#ins_username').text('@'+array[2]);
+                    $('#ins_title').text(array[0]);
+
+                    var key, count = 0;
+                    for(key in data.data) {
+                      if(data.data.hasOwnProperty(key)&&count<8) {
+                        $('#comment_content').append('<div class="comment_name">'+data.data[count].from.username+':'+'<p>'+data.data[count].text+'</p></div>');
+                        count++;
+                      }
+                    }
+                }
+            });  
+        }
+
+        function GetImageDetails(){
+         
+            $.ajax({     
+                type: 'GET',     
+                // url: 'https://api.instagram.com/oembed?url=http://instagram.com/p/tL68h3NUw-/',
+                url: 'https://api.instagram.com/oembed?url='+window.media_href, 
+                cache: false,     
+                dataType: 'jsonp',     
+                success: function(data) {           
+                    try{    
+                            
+                            window.array[0] = data.title;
+                            window.array[1] = data.media_id;
+                            window.array[2] = data.author_name;
+                            $('#comment_content').empty();
+                            GetUserDetails(array);
+                   
+                    }catch(err){} 
+                }
+            });
+
+        }
 
 
-$('#pinit_submit').click(function(){
-	pinterest_tag=$('#pinit_input').val();
-	flag = 2;
-	// pinterest_link=$('#recent_content').find('div').attr('display','none');
-	// pinterest_type=$('#recent_content').find('a').attr('data-pin-do');
-	// alert(pinterest_link);
-	// $('#recent_content').find('div').attr('display','none');
-		window.location.reload(true);
-});
+
 
 
